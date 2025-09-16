70a51e9b5ef1d449be2d8d61e847c770f4dcc03d
JASTI <jas302004@gmail.com>
2025-09-16 10:59:38 +0530

    Time: 2 ms (72.37%), Space: 61.9 MB (41.71%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -8,3 +8,16 @@ class Solution {
 }
         return max;
 }}
+/*
+Detailed trace for p = [7,1,5,3,6,4]:
+
+s	p	mv (after step)	profit if sell today = price - minPrice	maxProfit (after step)
+0	7	7	         	0
+1	1	1		        0
+2	5	1	5 - 1 = 4	4
+3	3	1	3 - 1 = 2	4
+4	6	1	6 - 1 = 5	5
+5	4	1	4 - 1 = 3	5
+
+Return 5. (Buy at 1, sell at 6.)
+*/
